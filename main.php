5bba9903e2fb5f339180a5bd6a1612dbdf688a1f
luzieltillo230-tech <luzieltillo230@gmail.com>
2026-04-13 19:15:10 +0800

    Add files via upload

---
+++ main.php
@@ -0,0 +1,23 @@
+<?php include "db1.php"?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>studen list</h2>
+    <a href="add.php" Add Student</a>
+    <<table>
+        <tr>
+            <th> ID</th>
+         <th>name</th>
+        <th>course</th>
+        <th>action</th>
+
+        </tr>
+    </table>
+
+</body>
+</html>
